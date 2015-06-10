9e7f3d1a484368e0b8e089c45f1755a8fd7ebbc8
meckert <meckert@iqnection.com>
2015-06-10 15:01:51 -0400

    Permissions update

--- code/AlbumPage.php
+++ code/AlbumPage.php
@@ -59,6 +59,7 @@
 		public function canCreate($member = null) { return true; }
 		public function canDelete($member = null) { return true; }
 		public function canEdit($member = null) { return true; }
+		public function canView($member = null) { return true; }
 				
 		public function GetThumbURL()
 		{
